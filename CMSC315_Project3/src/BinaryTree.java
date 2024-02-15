1d141d44059f27cdfa6558d6cc8e28c495c76eb9
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-02-15 12:16:46 -0500

    Basic code filled in.

--- CMSC315_Project3/src/BinaryTree.java
+++ CMSC315_Project3/src/BinaryTree.java
@@ -4,16 +4,52 @@
  * Description: 
  */
 
+import java.util.*;
+
 public class BinaryTree {
+	//no-arg constructor for best practice
+	public BinaryTree() {
+		
+	}
 	//Constructor that takes a string and constructs a tree
+	public BinaryTree(String userInput) {
+		
+	}
 	
 	//Construcor that takes in an AL of ints and makes a balanced tree 
+	//Note: unsure the type to use for ArrayList (Integer? String?)
+	public BinaryTree(ArrayList<Integer> bstList) {
+		
+	}
 	
 	//Outputs the indented tree 
+	public void displayTree() {
+		
+	}
 	
 	//determines if it's a binary search tree
+	public boolean isBST() {
+		return false;
+	}
+	
+	//maybe use another method to determine if the BST is balanced?
+	public boolean isBalanced() {
+		return false;
+	}
 	
 	//get the height of the bst
+	public int height() {
+		return -1;
+	}
+	
+	public int height(TreeNode<E> root) {
+		return -1;
+	}
 	
 	//returns an arraylist of values from the tree (use this in the second constructor?
+	//Again - unsure of what type the ArrayList should be? 
+	public ArrayList<Integer> toArray() {
+	
+		return null;
+	}
 }
--- CMSC315_Project3/src/DetermineBalancedBST.java
+++ CMSC315_Project3/src/DetermineBalancedBST.java
@@ -10,12 +10,19 @@ public class DetermineBalancedBST {
 
 	public static void main(String[] args) {
 		//Take in user input
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a binary tree: ");
+		String bstInput = input.nextLine();
+		
 		//Call tree constructor to construct a tree
+		
 		//Display the tree
+		
 		//Determine if the tree is a bst or balanced, etc
 			//If it is ask if user wants to input more binary search trees
 			//If not call the arraylist method and call the constructor with the al values to recreate a balanced/proper tree
 				//Report height of original tree and rebalanced tree
+		
 		//Ask user if want to recreate more trees
 
 	}
