d737b81e2b8fa30ac8e611338ae1877c750cb6fa
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-02-17 12:21:22 -0500

    Added code for Node, and beginning to create the tree from a string

--- CMSC315_Project3/src/BinaryTree.java
+++ CMSC315_Project3/src/BinaryTree.java
@@ -7,13 +7,16 @@
 import java.util.*;
 
 public class BinaryTree {
+	private Node root;
+	
 	//no-arg constructor for best practice
 	public BinaryTree() {
 		
 	}
+	
 	//Constructor that takes a string and constructs a tree
 	public BinaryTree(String userInput) {
-		
+		createTree(userInput);
 	}
 	
 	//Construcor that takes in an AL of ints and makes a balanced tree 
@@ -22,8 +25,17 @@ public class BinaryTree {
 		
 	}
 	
+	private void createTree(String userInput) {
+		//get rid of outside parantheses
+		userInput = userInput.substring(1, userInput.length()-1);
+		System.out.print(userInput);
+		
+		
+	}
+	
+	
 	//Outputs the indented tree 
-	public void displayTree() {
+	public void displayTree(String userInput) {
 		
 	}
 	
@@ -51,5 +63,50 @@ public class BinaryTree {
 	public ArrayList<Integer> toArray() {
 	
 		return null;
+	}
+	
+	private class Node {
+		Integer element;
+		Node leftChild;
+		Node rightChild;
+		
+		//no-arg constructor
+		private Node() {
+			
+		}
+		
+		private Node(Integer e) {
+			this.element = e;
+			this.leftChild = null;
+			this.rightChild = null;
+		}
+		
+//		private void insert(Node node, Integer newEl) {
+//			if(n)
+//			if(newEl > node.element) {
+//				if(node.rightChild != null) {
+//					insert(node.rightChild, newEl);
+//				}
+//				else {
+//					node.rightChild = newEl;
+//				}
+//			}
+//			else if(newEl < node.element)
+//		}
+//		
+		private void setLeftChild(Node left) {
+			this.leftChild = left;
+		}
+		
+		private Node getLeftChild() {
+			return leftChild;
+		}
+		
+		private void setRightChild(Node right) {
+			this.rightChild = right;
+		}
+		private Node getRightChild() {
+			return rightChild;
+		}
 	}
 }
